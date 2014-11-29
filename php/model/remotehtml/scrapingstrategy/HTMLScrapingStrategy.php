9a4694a67b3488042e887ebba0dfc4f138804346
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-29 13:36:12 -0500

    Index scraping strategies at base 1 to make django happy

--- php/model/remotehtml/scrapingstrategy/HTMLScrapingStrategy.php
+++ php/model/remotehtml/scrapingstrategy/HTMLScrapingStrategy.php
@@ -2,6 +2,6 @@
 
 
 abstract class HTMLScrapingStrategy {
-    const ELEMENT_PER_NEWS_ENTRY = 0;
-    const NEWS_ENTRIES_LISTED_IN_CONTAINING_ELEMENT = 1;
+    const ELEMENT_PER_NEWS_ENTRY = 1;
+    const NEWS_ENTRIES_LISTED_IN_CONTAINING_ELEMENT = 2;
 } 
