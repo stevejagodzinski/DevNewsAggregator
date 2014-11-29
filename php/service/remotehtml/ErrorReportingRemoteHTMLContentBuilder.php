44ed2b10ccdf5dd0135a7af74a9fcb864dde8b12
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-29 12:20:35 -0500

    Connect to postgres

--- php/service/remotehtml/ErrorReportingRemoteHTMLContentBuilder.php
+++ php/service/remotehtml/ErrorReportingRemoteHTMLContentBuilder.php
@@ -10,30 +10,25 @@ require_once 'php/view/remotehtml/HTMLResponseBuilder.php';
 class ErrorReportingRemoteHTMLContentBuilder {
 
     public static function getRemoteHTMLContent() {
-        $connection = mysqli_connect("localhost", "DevNews", "DevNews", "DevNewsAggregator");
-        if (mysqli_connect_errno()) {
-            $error = "Failed to connect to MySQL: " . mysqli_connect_error();
-            error_log($error);
-            return error;
-        } else {
-            $allStories = array();
+        $connection = pg_connect("host=localhost port=5432 dbname=DevNewsAggregator user=DevNews password=DevNews") or die("Could not connect to Postgres");
+        $result = pg_query($connection, 'SELECT * FROM "DevNewsAggregatorConfiguration_htmlcontent"') or die("Could not execute query");
 
-            $result = mysqli_query($connection, "SELECT * FROM htmlcontent WHERE enabled = 1");
-            while($row = mysqli_fetch_array($result)) {
-                $dateFieldInformation = DateFieldInformationFactory::create($row);
-                $remoteHTMLContent = new RemoteHTMLContent($row['url'], $row['name'], $row['scraping_strategy'], $row['outerContentSelector'], $row['innerContentSelector'], $row['titleSelector'],
-                    $dateFieldInformation, $row['ignore_first_n_posts'], $row['ignore_last_n_post']);
+        $allStories = array();
 
-                $fetchedStories = RemoteHTMLContentFetcher::getInstance()->fetch($remoteHTMLContent);
-                $allStories = array_merge($allStories, $fetchedStories);
-            }
+        while($row = pg_fetch_array($result)) {
+            $dateFieldInformation = DateFieldInformationFactory::create($row);
+            $remoteHTMLContent = new RemoteHTMLContent($row['url'], $row['name'], $row['scraping_strategy'], $row['outer_content_selector'], $row['inner_content_selector'], $row['title_selector'],
+                $dateFieldInformation, $row['ignore_first_n_posts'], $row['ignore_last_n_posts']);
 
-            mysqli_close($connection);
-
-            uasort($allStories, "NewsEntryComparator::compare");
-
-            return self::toHtml($allStories);
+            $fetchedStories = RemoteHTMLContentFetcher::getInstance()->fetch($remoteHTMLContent);
+            $allStories = array_merge($allStories, $fetchedStories);
         }
+
+        pg_close($connection);
+
+        uasort($allStories, "NewsEntryComparator::compare");
+
+        return self::toHtml($allStories);
     }
 
     private static function toHtml($newsStories) {
