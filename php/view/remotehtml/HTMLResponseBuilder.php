1aeacd91dfce328c3033c48341c2b7bc1540b603
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-30 19:30:30 -0500

    Add ISO date to HTML response

--- php/view/remotehtml/HTMLResponseBuilder.php
+++ php/view/remotehtml/HTMLResponseBuilder.php
@@ -6,7 +6,7 @@ class HTMLResponseBuilder {
             "<div class=\"news-entry\" data-content-source=\"". $newsEntry->getSource() ."\">".
                 "<div class=\"news-entry-header\">".
                     "<span class=\"news-entry-title\">".$newsEntry->getTitle()."</span>".
-                    "<span class=\"news-entry-date\">".date("F jS, Y h:i A", $newsEntry->getDate())."</span>".
+                    "<span class=\"news-entry-date\" data-iso-date=\"". date("c", $newsEntry->getDate()) ."\">".date("F jS, Y h:i A", $newsEntry->getDate())."</span>".
                 "</div>".
                 "<div class=\"news-entry-content\">".$newsEntry->getContent()."</div>".
             "</div>";
